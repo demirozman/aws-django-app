692578f64a07eac8b93c327b6a532b1b8520616f
Osman DEMIR <130417172+demirozman@users.noreply.github.com>
2023-09-09 21:03:45 +0300

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -3,8 +3,9 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
+USER=YYYYYYYY
 TOKEN=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-git clone https://$TOKEN@github.com/okti3535/aws-capstone-project-de05.git
+git clone https://$TOKEN@github.com/$USER/aws-capstone-project-de05.git
 cd /home/ubuntu/aws-capstone-project-de05
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
